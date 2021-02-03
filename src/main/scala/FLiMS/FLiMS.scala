2331bfb6dd53a5a6a7cbbad8bd29502d7422c08f
ikanoano <ikanoano@users.noreply.github.com>
2021-02-03 22:09:30 +0900

    fix design

--- src/main/scala/FLiMS/FLiMS.scala
+++ src/main/scala/FLiMS/FLiMS.scala
@@ -8,83 +8,84 @@ import chisel3.experimental.BundleLiterals._
 import scala.collection._
 
 /**
-  * Compute GCD using subtraction method.
-  * Subtracts the smaller from the larger until register y is zero.
-  * value in register x is then the GCD
+  * Merge size-way sequences of input records
   */
 
 object Config {
-  val KeyWidth = 32
-  val ValueWidth = 32
+  val KeyWidth    = 32
+  val ValueWidth  = 32
+  val TotalWidth  = KeyWidth + ValueWidth
 }
 class KVS extends Bundle {
   val key           = UInt(Config.KeyWidth.W)
   val value         = UInt(Config.ValueWidth.W)
 }
 
-
 class FLiMS (size: Int) extends Module {
   val io = IO(new Bundle {
-    val setI  = Vec(size, Flipped(Decoupled(new KVS()))) // input KVS set A
-    val setO  = Decoupled(Vec(size, new KVS()))          // output KVS set
+    val setI  = Vec(size, DeqIO(new KVS()))   // input size-way sequence of sorted KVS
+    val setO  = EnqIO(Vec(size, new KVS()))   // output sequence of sorted KVS
   })
 
   val pm      = Module(new ParallelMerger(size))
-
   pm.io.setI        := io.setI
-  pm.io.setO.foreach{ i => i.ready := io.setO.ready }
+  pm.io.setO.foreach{ i/*:EnqIO*/ => i.ready := io.setO.ready }
+
   io.setO.valid     := pm.io.setO(0).valid;
-  io.setO.bits      := pm.io.setO.map{ i => i.bits }
+  io.setO.bits      := pm.io.setO.map{ i/*:EnqIO(KVS)*/ => i.bits }
 }
 
 class ParallelMerger (size: Int) extends Module {
   val io = IO(new Bundle {
-    val setI  = Vec(size, Flipped(Decoupled(new KVS()))) // input KVS set A
-    val setO  = Vec(size,         Decoupled(new KVS()))  // output KVS set
+    val setI  = Vec(size, DeqIO(new KVS()))   // input size-way sequence of sorted KVS set
+    val setO  = Vec(size, EnqIO(new KVS()))   // input size-way sequence of sorted KVS set
   })
 
-  val setA = Wire(Vec(size/2, Flipped(Decoupled(new KVS()))))  // input KVS set A
-  val setB = Wire(Vec(size/2, Flipped(Decoupled(new KVS()))))  // input KVS set B
+  val setA = Wire(Vec(size/2, DeqIO(new KVS())))  // input KVS set A
+  val setB = Wire(Vec(size/2, DeqIO(new KVS())))  // input KVS set B
 
   if (size > 2) {
     val pmA = Module(new ParallelMerger(size/2))
     val pmB = Module(new ParallelMerger(size/2))
-    pmA.io.setI  := io.setI.take(size/2)
-    pmB.io.setI  := io.setI.drop(size/2)
-    setA                        := pmA.io.setO.map(i => Flipped(i))
-    setB                        := pmB.io.setO.map(i => Flipped(i))
+    pmA.io.setI   := io.setI.take(size/2)
+    pmB.io.setI   := io.setI.drop(size/2)
+    setA          := pmA.io.setO.map(i => Flipped(i))
+    setB          := pmB.io.setO.map(i => Flipped(i))
   } else {
     assert(size == 2)
-    setA      := io.setI.take(1)
-    setB      := io.setI.drop(1)
+    setA          := io.setI.take(1)
+    setB          := io.setI.drop(1)
   }
 
   // queues storing setA/setB
-  val qA  = setA.map(i => Queue(i, 8))  // qA(0) is a DecoupledIO
-  val qB  = setB.map(i => Queue(i, 8))
+  val qA  = setA.map(i => Queue(i, 8))  // qA(0) is a DeqIO of deq
+  val qB  = setB.map(i => Queue(i, 8)).reverse
+  val qAValid = qA.map(i => i.valid).reduce(_&&_) // all queues in qA are valid
+  val qBValid = qB.map(i => i.valid).reduce(_&&_)
 
   // head of qA/aB
   val a   = qA.map(i => i.bits)
   val b   = qB.map(i => i.bits)
-  val cmp = for(i <- 0 until size) yield a(i).key > b.reverse(i).key
-
-  // dequeue qA/qB into cA/cB
-  //qA.ready :=  cmp(size-1)
-  //qB.ready := !cmp(0)
-  (0 until size).foreach{ i =>
-    qA(i).ready :=  cmp(i)
-    qB(i).ready := !cmp(i)
-  }
 
   // dequeued records to be compared
-  val cA    = Vec(size, RegInit((new KVS()).Lit(_.key -> ~(0.U(Config.KeyWidth.W)))))
-  val cB    = Vec(size, RegInit((new KVS()).Lit(_.key -> ~(0.U(Config.KeyWidth.W)))))
-  //val cA    = Reg(Vec(size, new KVS()))
-  //val cB    = Reg(Vec(size, new KVS()))
-  // update if cA(i) or cB(i) has taken
+  // Note that cA and cB are the biggest value right after the reset so that they must be taken before any valid value in qA/qB
+  val cA        = Vec(size, RegInit((new KVS()).Lit(_.key -> ~(0.U(Config.KeyWidth.W)))))
+  val cB        = Vec(size, RegInit((new KVS()).Lit(_.key -> ~(0.U(Config.KeyWidth.W)))))
+  val cmp       = for(i <- 0 until size) yield cA(i).key > cB(i).key
+
+  // stall?
+  // For simplicity, all registers will stall if (1) output is not ready (2) any input queue is not valid
+  val isOutReady= io.setO(0).ready && io.setO(size-1).ready // either FIFO is taken first
+  val stall     = ~isOutReady || ~qAValid || ~qBValid
+  val deqA      = cmp.map( _ && ~stall)
+  val deqB      = cmp.map(~_ && ~stall)
+
   (0 until size).foreach{ i =>
-    when(cmp(i))  {cA(i) := a(i)}
-    .otherwise    {cB(i) := b.reverse(i)}
+    // dequeue qA/qB into cA/cB
+    qA(i).ready := deqA(i);
+    qB(i).ready := deqB(i);
+    when(deqA(i))  {cA(i) := a(i)}
+    when(deqB(i))  {cB(i) := b(i)}
   }
 
   // sorting network
@@ -92,18 +93,16 @@ class ParallelMerger (size: Int) extends Module {
   // collect half records that are bigger than others
   sn.io.setI   := VecInit.tabulate(size)(i => Mux(cmp(i), cA(i), cB(i)))
 
-  // output a KVS bundle if setO for all records is ready
-  val isOutReady  = io.setO.map(i => i.ready).reduce((z,n) => z & n)
+  // output a size-way KVS set
   (0 until size).foreach{ i =>
-    io.setO(i).bits := sn.io.setO(i)
-    io.setO(i).valid:= isOutReady
+    io.setO(i).bits  := sn.io.setO(i)
+    io.setO(i).valid := ~stall
   }
 }
 
-
 class SortingNetwork(size: Int) extends Module {
   val io = IO(new Bundle {
-    val setI          = Input(Vec(size, new KVS())) // input KVS set
+    val setI          = Input (Vec(size, new KVS())) // input KVS set
     val setO          = Output(Vec(size, new KVS())) // output KVS set
   })
 
